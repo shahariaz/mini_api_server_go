fc98bf918457c6dd52c3e60e0818c161046a9ed0
shahariaz <shahariaz.info@gmail.com>
2025-01-25 21:37:19 +0600

    finished a basic static site

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shahriaz/mini_api_server
+
+go 1.23.5
--- main.go
+++ main.go
@@ -6,5 +6,36 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter,r *http.Request){
+if r.URL.Path != "/hello"{
+	http.Error(w,"404 not Found",http.StatusNotFound)
+	return
+}
+if r.Method != "Get" {
+	http.Error(w,"Method not supported",http.StatusNotFound)
+	return
+}
+fmt.Fprintf(w,"Hello")
+}
 
-func main(){}
+func formHandler(w http.ResponseWriter, r *http.Request){
+if err := r.ParseForm(); err != nil{
+	fmt.Fprintf(w,"ParseForm failed: %v ",err)
+	return
+}
+  fmt.Fprintf(w,"Form submitted with data: %v",r.Form)
+	name := r.FormValue("name")
+	address:= r.FormValue("address")
+	fmt.Fprintf(w,"Name: %s\n",name)
+	fmt.Fprintf(w,"Address: %s\n",address)
+}
+func main(){
+	fileServer := http.FileServer(http.Dir("./static/"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello",helloHandler)
+	fmt.Printf("starting server at http://localhost:8080")
+	if err:= http.ListenAndServe(":8000",nil); err != nil{
+		log.Fatal(err)
+	}
+}
